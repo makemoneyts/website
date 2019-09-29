296a33548f69ff16119a530692cf69416c7fd172
Nakanishi, Yusuke <nakanishiyusuke0000@gmail.com>
2019-09-29 15:17:55 +0900

    ざっくりコミット

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
+import React from "react";
+
+const style = css`
+  background: red;
+`;
+
+type Props = {
+  name: string;
+};
+
+const MyApp: React.FC<Props> = ({ name }) => <div>{name}</div>;
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div css={style} className="App">
+      MakeMoney.TS
     </div>
   );
-}
+};
 
 export default App;
