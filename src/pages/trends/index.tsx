7f6fe05d81a71c3bca068e1354087a820077cd18
Nakanishi, Yusuke <nakanishiyusuke0000@gmail.com>
2019-10-13 22:54:13 +0900

    Revert "routing の追加"
    This reverts commit 83955cd2

--- src/pages/trends/index.tsx
+++ src/pages/trends/index.tsx
@@ -1,20 +1,16 @@
 /** @jsx jsx */
 import { jsx, css } from "@emotion/core";
 import React from "react";
-import { Link } from "react-navi";
 
 const wrapperStyle = css`
   margin-top: 10vw;
   background: whitesmoke;
   color: gray;
-  display: flex;
-  flex-direction: column;
 `;
 
 const Trends: React.FC<{}> = () => {
   return (
     <div css={wrapperStyle}>
-      <Link href={`/why`}>なぜ今 makeMoney なのか</Link>
       <a
         css={css`
           color: gray;
