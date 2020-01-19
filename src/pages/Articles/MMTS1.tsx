443802b7c3748409bb547c63b41f33c0beedac1c
Nakanishi, Yusuke <nakanishiyusuke0000@gmail.com>
2020-01-19 17:27:40 +0900

    無駄に three.js を入れた

--- src/pages/Articles/MMTS1.tsx
+++ src/pages/Articles/MMTS1.tsx
@@ -1,6 +1,7 @@
 /** @jsx jsx */
 import { jsx, css } from "@emotion/core";
 import React from "react";
+import * as THREE from "three";
 
 const wrapperStyle = css`
   background: whitesmoke;
@@ -8,6 +9,36 @@ const wrapperStyle = css`
 `;
 
 const MMTS1: React.FC<{}> = () => {
+  var scene = new THREE.Scene();
+  var camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+
+  var renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  var geometry = new THREE.BoxGeometry(1, 1, 1);
+  var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  var cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  camera.position.z = 5;
+
+  var animate = function() {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+  };
+
+  animate();
+
   return (
     <div css={wrapperStyle}>
       第一回 MakeMomoney.TS終了。
--- src/pages/Articles/MMTS2.tsx
+++ src/pages/Articles/MMTS2.tsx
@@ -1,6 +1,7 @@
 /** @jsx jsx */
 import { jsx, css, Global } from "@emotion/core";
 import React from "react";
+import * as THREE from "three";
 
 const wrapperStyle = css`
   background: whitesmoke;
@@ -16,6 +17,36 @@ const globalStyle = css`
 `;
 
 const MMTS1: React.FC<{}> = () => {
+  var scene = new THREE.Scene();
+  var camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+
+  var renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  var geometry = new THREE.BoxGeometry(1, 1, 1);
+  var material = new THREE.MeshBasicMaterial({ color: "#e5b913" });
+  var cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  camera.position.z = 5;
+
+  var animate = function() {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+  };
+
+  animate();
+
   return (
     <div css={wrapperStyle}>
       <Global styles={globalStyle} />
