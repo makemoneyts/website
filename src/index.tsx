238d3286f345cd91f8b67d6b8cf38a76817b5f45
Nakanishi, Yusuke <nakanishiyusuke0000@gmail.com>
2020-01-19 15:19:39 +0900

    ルーティングを追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let Home = () => <div>Home</div>;
+let Dash = () => <div>Dash</div>;
+
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route path="/about">
+        <Home />
+      </Route>
+      <Route path="/users">
+        <Dash />
+      </Route>
+      <Route path="/">
+        <App />
+      </Route>
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
